d1e4b2aabf7f6c0d7087303a21a865166270bce8
Fanaen <contact@fanaen.fr>
2015-08-23 00:34:43 +0200

    Improve selection system

--- app/scripts/classes/network.js
+++ app/scripts/classes/network.js
@@ -9,6 +9,7 @@ var Network = function () {
   // -- Attributes --
   this.width = 960;
   this.height = 500;
+  this.selected = undefined;
 
   this.svg = {};
   this.vis = {};
@@ -87,30 +88,41 @@ var Network = function () {
 
     // Update nodes --
     var node = this.vis.selectAll(".node")
-      .data(graph.nodes)
-      .enter().append("circle")
+      .data(graph.nodes);
+
+    var node_enter = node.enter().append("circle")
       .attr("class", "node")
       .attr("r", 10)
-      .style("fill", function(d) { return Network.getInstance().color(d.group); })
       .on("click", function(d) {
+
+        // Selection system --
+        var network = Network.getInstance();
+        if(network.selected != undefined) network.selected.selected = false;
+        d.selected = true;
+        network.selected = d;
+        network.restyleNodes(node);
+
         if (d3.event.defaultPrevented) return; /* ignore drag */
+
+        // Transfer to the game engine --
         Game.getInstance().selectNode(d);
-        console.log('Done!');
+
       })
+      .on("mousedown.zoom", function() {d3.event.stopPropagation(); }) // Ignore drag/zoom --
       .call(this.force.drag);
 
-    // Ignore drag/zoom --
-    node.on("mousedown.zoom", function() {d3.event.stopPropagation(); });
+    this.restyleNodes(node);
 
-    node.append("title")
+    node_enter.append("title")
       .text(function(d) { return d.name; });
 
     // Update nodes --
     var ghost = this.vis.selectAll(".ghost")
       .data(graph.ghosts)
-      .enter().append("circle")
+      .enter().append("rect")
       .attr("class", "ghost")
-      .attr("r", 13);
+      .attr("width", 20)
+      .attr("height", 20);
 
     this.force.on("tick", function() {
       link
@@ -124,16 +136,26 @@ var Network = function () {
         .attr("cy", function(d) { return d.y; });
 
       ghost
-        .attr("cx", function(d) {
-          return Network.getInstance().vis.selectAll(".node").filter(function(df, i) { return d.node == i; }).attr("cx");
+        .attr("x", function(d) {
+          return Network.getInstance().vis.selectAll(".node").filter(function(df, i) { return d.node == df.id; }).attr("cx") - 34;
         })
-        .attr("cy", function(d) {
-          return Network.getInstance().vis.selectAll(".node").filter(function(df, i) { return d.node == i; }).attr("cy");
+        .attr("y", function(d) {
+          return Network.getInstance().vis.selectAll(".node").filter(function(df, i) { return d.node == df.id; }).attr("cy") - 10 ;
         })
     });
 
     Game.getInstance().loadData(graph);
   };
+
+  this.restyleNodes = function(node) {
+    var fill = function(d) {
+      if(d.dominated == 0) { return "#E74C3C"; }
+      else { return Network.getInstance().color(d.group); } }
+
+    node
+      .attr("fill", fill)
+      .style("stroke", function(d) { return d.selected ? "#000" : "#263d41"; } );
+  }
 };
 
 Network.getInstance = function () { return Network._instance || new Network(); }
