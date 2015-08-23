239031dac32131632d625acf25847e6f0ad3b101
Fanaen <contact@fanaen.fr>
2015-08-23 19:29:07 +0200

    Improve sound controls and intro

--- app/scripts/classes/sound.js
+++ app/scripts/classes/sound.js
@@ -30,6 +30,10 @@ var Sound = function () {
         this.loopMute = true;
       if(localStorage.getItem("soundsMute") === "true")
         this.soundsMute = true;
+      if(localStorage.getItem("loopVolume") != null)
+        this.loopVolume = parseFloat(localStorage.getItem("loopVolume"));
+      if(localStorage.getItem("soundsVolume") != null)
+        this.soundsVolume = parseFloat(localStorage.getItem("soundsVolume"));
     }
 
     this.loops.push(new Howl({
@@ -71,11 +75,17 @@ var Sound = function () {
       var sound = Sound.getInstance();
       sound.setLoopMute(!sound.loopMute);
     })
+    $('#loopPlus').on('click', function() { Sound.getInstance().loopPlus(); });
+    $('#loopLess').on('click', function() { Sound.getInstance().loopLess(); });
+    $('#soundsPlus').on('click', function() { Sound.getInstance().soundsPlus(); });
+    $('#soundsLess').on('click', function() { Sound.getInstance().soundsLess(); });
+
+    $('#soundsMute').text(this.soundsMute ? "Unmute" : "Mute");
+    $('#loopMute').text(this.loopMute ? "Unmute" : "Mute");
   };
 
   this.newTrack = function() {
     this.currentLoopId = Math.floor(Math.random() * 100) % this.loops.length;
-    console.log(this.currentLoopId);
     this.currentLoop = this.loops[this.currentLoopId];
     this.currentLoop.volume(this.loopVolume);
 
@@ -84,6 +94,7 @@ var Sound = function () {
   };
 
   this.onMove = function() {
+    console.log(this.soundsMute);
     if(!this.soundsMute) {
       this.sounds.volume(this.soundsVolume);
       this.sounds.play('move');
@@ -118,6 +129,7 @@ var Sound = function () {
     }
 
     this.soundsMute = boolean;
+    $('#soundsMute').text(this.soundsMute ? "Unmute" : "Mute");
   };
 
   this.setLoopMute = function(boolean) {
@@ -126,6 +138,7 @@ var Sound = function () {
     }
 
     this.loopMute = boolean;
+    $('#loopMute').text(this.loopMute ? "Unmute" : "Mute");
 
     if(this.loopMute) {
       this.currentLoop.pause();
@@ -133,6 +146,46 @@ var Sound = function () {
       this.currentLoop.play();
     }
   };
+
+  this.soundsPlus = function() {
+    this.soundsVolume += 0.1;
+    if(this.soundsVolume > 1) this.soundsVolume = 1;
+
+    if(typeof(Storage) !== "undefined") {
+      localStorage.setItem("soundsVolume", this.soundsVolume.toFixed(1).toString());
+    }
+  }
+
+  this.soundsLess = function() {
+    this.soundsVolume -= 0.1;
+    if(this.soundsVolume < 0) this.soundsVolume = 0;
+
+    if(typeof(Storage) !== "undefined") {
+      localStorage.setItem("soundsVolume", this.soundsVolume.toFixed(1).toString());
+    }
+  }
+
+  this.loopPlus = function() {
+    this.loopVolume += 0.1;
+    if(this.loopVolume > 1) this.loopVolume = 1;
+
+    this.currentLoop.volume(this.loopVolume);
+
+    if(typeof(Storage) !== "undefined") {
+      localStorage.setItem("loopVolume", this.loopVolume.toFixed(1).toString());
+    }
+  }
+
+  this.loopLess = function() {
+    this.loopVolume -= 0.1;
+    if(this.loopVolume < 0) this.loopVolume = 0;
+
+    this.currentLoop.volume(this.loopVolume);
+
+    if(typeof(Storage) !== "undefined") {
+      localStorage.setItem("loopVolume", this.loopVolume.toFixed(1).toString());
+    }
+  }
 };
 
 Sound.getInstance = function () { return Sound._instance || new Sound(); }
